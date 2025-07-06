b610452d885bbd0e1a4e9b41069171cf769669e9
Max Walley <58304135+maxwalley@users.noreply.github.com>
2025-07-06 23:55:21 +0100

    Changed some syntax for fixed term meter

--- src/FixedTermLoudnessMeter.cpp
+++ src/FixedTermLoudnessMeter.cpp
@@ -75,12 +75,12 @@ float FixedTermLoudnessMeter::getLoudness() const
         accumulatedChannels += processor.getCurrentBlockMeanSquares() * processor.weighting;
     });
 
-    return -0.691 + 10 * log10(accumulatedChannels);
+    return -0.691f + 10.0f * log10(accumulatedChannels);
 }
 
 int FixedTermLoudnessMeter::getBlockLengthSamples(const std::chrono::milliseconds& windowLength) const
 {
-    return (windowLength.count() / 1000.0) * sampleRate;
+    return (windowLength.count() / 1000.0f) * sampleRate;
 }
     
 }
