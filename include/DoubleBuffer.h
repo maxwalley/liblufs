9ecac2e6045cb80ecc8c703034eb0653538784ac
Max Walley <58304135+maxwalley@users.noreply.github.com>
2025-08-12 21:39:10 +0100

    Added DoubleBuffer class

---
+++ include/DoubleBuffer.h
@@ -0,0 +1,83 @@
+/*
+This code was inspired by the farbot RealtimeObject implementation. See https://github.com/hogliux/farbot for a more developed approach
+*/
+
+#pragma once
+
+#include <atomic>
+#include <array>
+#include <cassert>
+
+template<typename T>
+class DoubleBuffer
+{
+public:
+    DoubleBuffer(const T& initialVal)  : buffers{{initialVal}, {initialVal}}, realtimeCopy{initialVal} {}
+
+    ~DoubleBuffer()
+    {
+        //Busy bit should not be set
+        assert((control.load() & BusyBit) == 0);
+    }
+
+    T& realtimeAquire()
+    {
+        return realtimeCopy;
+    }
+
+    void realtimeRelease()
+    {
+        //Set the busy bit and get the write index
+        const int writeIndex = control.fetch_or(BusyBit) & IndexBit;
+        buffers[writeIndex] = realtimeCopy;
+
+        //Unset the busy bit, set the new data bit
+        control.store((writeIndex & IndexBit) | NewDataBit);
+    }
+
+    T& nonRealtimeRead()
+    {
+        int current = control.load();
+
+        if(!current & NewDataBit)
+        {
+            return;
+        }
+
+        int newValue;
+
+        //CAS loop to spin while busy bit is set, then set the new buffer to write to
+        do
+        {
+            current &= ~BusyBit;
+            newValue = (comp ^ IndexBit) & IndexBit;
+        }
+        while(!control.compare_exchange_weak(current, newValue));
+
+        //Return the last buffer that was written to
+        return buffers[(newValue & IndexBit) & 0x1]
+    }
+
+    //This is not realtime safe with the other calls
+    void reset(const T& newValue)
+    {
+        assert((control.load() & BusyBit) == 0);
+
+        buffers[0] = newValue;
+        buffers[1] = newValue;
+        realtimeCopy = newValue;
+    }
+
+private:
+    enum 
+    { 
+        IndexBit = (1 << 0), 
+        NewDataBit = (1 << 1), 
+        BusyBit = (1 << 2)
+    }
+
+    std::atomic<int> control;
+
+    std::array<T, 2> buffers;
+    T realtimeCopy;
+};
